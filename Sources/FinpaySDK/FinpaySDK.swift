91568a69bd6162f4fe8c69ba2d668cb14454d81a
skadskuds.f <skadskuds.f@gmail.com>
2024-05-08 13:09:36 +0700

    first commit

--- Sources/FinpaySDK/FinpaySDK.swift
+++ Sources/FinpaySDK/FinpaySDK.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+
+public struct FinpaySDK {
+    static func doSomeWork(){
+        print("Hello")
+    }
+}
